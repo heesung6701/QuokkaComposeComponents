befe59e989925494156ccc4613360b000068540b
윤희성 <heesung6701@naver.com>
2022-02-27 21:05:10 +0900

    fix some code for build error

--- ratingbar/src/androidTest/java/com/quokkaman/ratingbar/ExampleInstrumentedTest.kt
+++ ratingbar/src/androidTest/java/com/quokkaman/ratingbar/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.quokkaman.ratinbbar.test", appContext.packageName)
+        assertEquals("com.quokkaman.ratingbar.test", appContext.packageName)
     }
 }
--- ratingbar/src/main/java/com/quokkaman/ratingbar/ui/RatingStar.kt
+++ ratingbar/src/main/java/com/quokkaman/ratingbar/ui/RatingStar.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.quokkaman.ratingbar.R
 import com.quokkaman.ratingbar.FractionalRectangleShape
 
 @Composable
